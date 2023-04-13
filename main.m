d5004042f2ea4d4482116fc8786905734c64b4e3
nemanja731 <nemanja.jankovic731@gmail.com>
2023-04-13 06:25:15 +0200

    Update main file

--- main.m
+++ main.m
@@ -2,12 +2,12 @@ clear
 close all
 clc
 
-%% Dopuna nedostajucih vrednosti u bazi i prebacivanje csv fajla u matricu X
+%% Supplementing the missing values ​​in the database and transferring the csv file to the X matrix
 
 T = readtable('hepatitis.csv','TreatAsEmpty',{'?'});
 [numSamples, numAllAttributes] = size(T);
 numAllAttributes = numAllAttributes - 1;
-%zamena prve kolone(klasa) sa poslednjom(atribut) radi lakse analize
+%replacing the first column (class) with the last (attribute) for easier analysis
 for j = 1:numSamples
     t = T{j, 1};
     T{j, 1} = T{j, 20};
@@ -44,7 +44,7 @@ for i = 1:numAllAttributes
 end
 X = T{:, :};
 
-%% Prikaz klasa
+%% Class view
 
 numAttributes = 10;
 figure
@@ -56,7 +56,7 @@ figure
 
 R1 = corrcoef(X);
 [rho, indexesPearson] = sort(R1(end, 1:end - 1),'descend');
-% Kreacija promenljive ranga
+% Creation of a rank variable
 rank = sort(X);
 for i = 1:numAllAttributes + 1
     values = unique(X(:, i)); 
@@ -67,15 +67,14 @@ end
 R2 = corrcoef(rank);
 figure
     heatmap(R1);
-    title('Pearsonov koeficijent korelacije');
+    title('Pearsons correlation coefficient');
 figure
     heatmap(R2);
-    title('Spermanov koeficijent korelacije');
+    title('Spermans correlation coefficient');
     
-%% Informaciona dobit IG
+%% Information gain of IG
 
-% Prikaz histograma za svaki atribut, za odredjivanje
-% kvantizacionih nivoa kod kontinualnih atributa
+% Histogram display for each attribute, for determining quantization levels for continuous attributes
 %{
 for i = 1:19
     figure;
@@ -93,7 +92,7 @@ end
 [~, indexesInfoDA] = sort(InfoDA, 'descend');
 Z = X(:, sort(indexesInfoDA(1:numAttributes)));
 
-%% Podela na obucavajuci i testirajuci skup
+%% Division into training and testing set
 
 X1 = Z(X(:, end) == 1, :);
 X2 = Z(X(:, end) == 2, :);
@@ -107,7 +106,7 @@ X2o = X2(indX2(1:floor(percentage*end)), :);
 X1t = X1(indX1(floor(percentage*end) + 1:end), :);
 X2t = X2(indX2(floor(percentage*end) + 1:end), :);
 
-%% Redukcija dimenzija na bazi matrica rasejanja
+%% Dimension reduction based on scattering matrices
 
 M1 = mean(X1o)';
 M2 = mean(X2o)';
@@ -120,7 +119,7 @@ Sb = (M1 - M0)*(M1 - M0)'*p1 + (M2 - M0)*(M2 - M0)'*p2;
 Sm = Sb + Sw;
 T = Sw^-1*Sb;
 
-%% 2 dimenzije
+%% 2 dimensions
 
 [V, ~] = eigs(T, 2);
 A = real(V);
@@ -130,12 +129,12 @@ Y1t = A'*X1t';
 Y2t = A'*X2t';
 figure;
     plot(Y1o(1, :), Y1o(2, :), 'ko', Y2o(1, :), Y2o(2, :), 'rx');
-    title('LDA obucavajuceg skupa na 2 dimenzije')
+    title('LDA of the training set on 2 dimensions')
 figure;
     plot(Y1t(1, :), Y1t(2, :), 'ko', Y2t(1, :), Y2t(2, :), 'rx');
-    title('LDA testirajuceg skupa na 2 dimenzije')
+    title('LDA of the test set on 2 dimensions')
     
-%% 3 dimenzije
+%% 3 dimensions
 
 [V, ~] = eigs(T, 3);
 A = real(V);
@@ -145,16 +144,16 @@ Y1t = A'*X1t';
 Y2t = A'*X2t';
 figure;
     plot3(Y1o(1, :), Y1o(2, :), Y1o(3, :), 'ko', Y2o(1, :), Y2o(2, :), Y2o(3, :), 'rx');
-    title('LDA obucavajuceg skupa na 3 dimenzije')
+    title('LDA of the training set in 3 dimensions')
     grid on;
 figure;
     plot3(Y1t(1, :), Y1t(2, :), Y1t(3, :), 'ko', Y2t(1, :), Y2t(2, :), Y2t(3, :), 'rx');
-    title('LDA testirajuceg skupa na 3 dimenzije')
+    title('LDA of the test set in 3 dimensions')
     grid on;
     
-%% Testiranje hipoteza
+%% Hypothesis testing
 
-% skupovi su nam nebalansirani
+% sets are unbalanced
 c = zeros(2,2);
 a = sum(X(:,end) == 1);
 b = sum(X(:,end) == 2);
@@ -162,31 +161,31 @@ c11 = 0;
 c22 = 0;
 c21 = 1;
 c12 = 10;
-% za sad najbolje 1,10
+% the best for now is 1,10
 prag = -log((c22-c12)/(c11-c21));
 
 for i=1:length(X1t)
- % prolazimo kroz klasu X1_t i odredjujemo h(X1)
+ % we go through the class X1_t and determine h(X1)
  f1=(1/(2*pi*(det(S1))^0.5))*exp(-0.5*(X1t(i,:)'-M1)'*inv(S1)*(X1t(i,:)'-M1));
  f2=(1/(2*pi*(det(S2))^0.5))*exp(-0.5*(X1t(i,:)'-M2)'*inv(S2)*(X1t(i,:)'-M2));
  h=-log(f1)+log(f2);
- if h<prag % klasifikator donosi odluku da je klasa 1
+ if h<prag % the classifier decides that the class is 1
     c(1,1)=c(1,1)+1;
  else
-    % klasifikator gresi i misli da je klasa 2
+    % the classifier makes a mistake and thinks it is class 2
     c(2,1)=c(2,1)+1;
  end
 end
 
 for i=1:length(X2t)
- % prolazimo kroz klasu X2t i odredjujemo h(X2)
+ % we go through the class X2t and determine h(X2)
  f1=(1/(2*pi*(det(S1))^0.5))*exp(-0.5*(X2t(i,:)'-M1)'*inv(S1)*(X2t(i,:)'-M1));
  f2=(1/(2*pi*(det(S2))^0.5))*exp(-0.5*(X2t(i,:)'-M2)'*inv(S2)*(X2t(i,:)'-M2));
  h=-log(f1)+log(f2);
- if h<prag % klasifikator gresi i misli da je klasa 1
+ if h<prag % the classifier makes a mistake and thinks it is class 1
     c(1,2)=c(1,2)+1;
  else
-    % klasifikator doneo ispravnu odluku
+    % the classifier made the correct decision
     c(2,2)=c(2,2)+1;
  end
 end
@@ -194,38 +193,38 @@ end
 err = (c(1,2)+c(2,1))/sum(sum(c)) 
 c
 
-podaci = X;
+data = X;
 
-%% Transponovanje matrice i iscrtavanje klasa
-ulaz = podaci(:, 1:19)';
-klasa = podaci(:, 20)';
+%% Matrix transposition and class plotting
+input = data(:, 1:19)';
+klasa = data(:, 20)';
 N = length(klasa);
 
-K1 = ulaz(:, klasa == 1);
-K2 = ulaz(:, klasa == 2);
+K1 = input(:, klasa == 1);
+K2 = input(:, klasa == 2);
 
-izlaz = zeros(2, N);
+output = zeros(2, N);
 
-izlaz(1, klasa == 1) = 1;
-izlaz(2, klasa == 2) = 1;
+output(1, klasa == 1) = 1;
+output(2, klasa == 2) = 1;
 
-%% Podela na trening i test skup  
+%% Devision into training and test set  
 ind = randperm(N);
-indTrening = ind(1 : round(0.7*N));
+indTraining = ind(1 : round(0.7*N));
 indTest = ind(round(0.7*N+1) : N);
 
-ulazTrening = ulaz(:, indTrening);
-izlazTrening = izlaz(:, indTrening);
+inputTraining = input(:, indTraining);
+outputTraining = output(:, indTraining);
 
-ulazTest = ulaz(:, indTest);
-izlazTest = izlaz(:, indTest);
+inputTest = input(:, indTest);
+outputTest = output(:, indTest);
 
-%% Kreiranje NM
-arhitektura = [10 10 10];
-net = patternnet(arhitektura);
+%% Creation of Neural Network
+architecture = [10 10 10];
+net = patternnet(architecture);
 
-net.trainParam.epochs = 1000; %max epoha
-net.trainParam.goal = 1e-4; %ciljana greska
+net.trainParam.epochs = 1000; %max epoch
+net.trainParam.goal = 1e-4; %target error
 net.trainParam.min_grad = 1e-5;
 
 net.trainFcn = 'trainbfg';
@@ -234,10 +233,10 @@ net.divideFcn = '';
 net.performFcn = 'msereg'; %'crossentropy'; 
 net.performFcn = 'crossentropy'; %'crossentropy'; 
 net.performParam.regularization = 5e-5;
-%% Treniranje NM
-net = train(net, ulazTrening, izlazTrening);
-%% Performanse NM
-predTest = net(ulazTest);
-predTrening = net(ulazTrening);
-figure, plotconfusion(izlazTest, predTest)
-figure, plotconfusion(izlazTrening, predTrening)
+%% NM training
+net = train(net, inputTraining, outputTraining);
+%% NM performance
+predTest = net(inputTest);
+predTraining = net(inputTraining);
+figure, plotconfusion(outputTest, predTest)
+figure, plotconfusion(outputTraining, predTraining)
